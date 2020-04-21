c7eccad2bba28ce3359f37cf51aca1f1d5ecf19a
うｐﾊｼ <yukawallstudio@gmail.com>
2020-04-21 23:53:27 +0900

    Update ZenAutoPlayList.user.js

--- ZenAutoPlayList.user.js
+++ ZenAutoPlayList.user.js
@@ -6,26 +6,16 @@
 // @author       You
 // @match        https://www.nicovideo.jp/*
 // @grant        none
+// @require       https://raw.githubusercontent.com/wallstudio/XhrFetchInjection/master/xhrFetchInjection.user.js
 // ==/UserScript==
 
 
 (function() {
     'use strict';
 
-    let box = document.createElement("div");
-    box.style.display = "box";
-    box.style.position = "fixed";
-    box.style.bottom = 0;
-    document.body.appendChild(box);
 
-    let g_saveEntry = null;
-
-    let getPlayList = function()
+    let getPlayList = function(videos)
     {
-        let allPost = Array.from(document.getElementsByClassName("NicorepoTimelineItem"));
-        let enable = allPost.filter(e => getComputedStyle(e, null).display != "none");
-        let videos = enable.filter(e =>e.getAttribute("data-topic") == "nicovideo.user.video.upload");
-
         let jsonContainer = new Object();
         jsonContainer.items = [];
         for(let i = 0; i < videos.length; i++)
@@ -55,24 +45,41 @@
         }
 
         let json = JSON.stringify(jsonContainer);
-        let entry = document.createElement("a");
-        entry.style.margin = "5px";
-        entry.innerText = `Save(${jsonContainer.items.length})`;
-        entry.href = 'data:application/json,' + encodeURIComponent(`${json}`);
-        entry.download = `${jsonContainer.items[0].title}-${jsonContainer.items[jsonContainer.items.length - 1].title}.playlist.json`;
-        box.appendChild(entry);
-
-        if(g_saveEntry != null)
-        {
-            g_saveEntry.parentNode.removeChild(g_saveEntry);
-        }
-        g_saveEntry = entry;
-        console.log(json);
+        let href = 'data:application/json,' + encodeURIComponent(`${json}`);
+        let download = `${jsonContainer.items[0].title}-${jsonContainer.items[jsonContainer.items.length - 1].title}.playlist.json`;
+        return { href : href, download : download};
     }
 
-    let entry = document.createElement("a");
-    entry.style.margin = "5px";
-    entry.innerText = "ZenAutoPlayList";
-    entry.addEventListener("click", getPlayList);
-    box.appendChild(entry);
+    let doneList = [];
+    injectFetch((url, args, callback) =>
+    {
+        let ret = callback(args);
+
+        let allPost = Array.from(document.getElementsByClassName("NicorepoTimelineItem"));
+        let enable = allPost.filter(e => getComputedStyle(e, null).display != "none");
+        let videos = enable.filter(e =>e.getAttribute("data-topic") == "nicovideo.user.video.upload");
+        for(let i = 1; i < videos.length; i++)
+        {
+            let last = videos[i - 1];
+            if(doneList.includes(last))
+            {
+                continue;
+            }
+
+            doneList.push(last);
+            let partVideos = videos.slice(0, i);
+            let playListData = getPlayList(partVideos);
+            console.log(`${partVideos.length} ${playListData.download}`);
+
+            let button = document.createElement("a");
+            button.innerText = `  PlayList(${partVideos.length})`;
+            button.href = playListData.href;
+            button.download = playListData.download;
+            let footer = last.getElementsByClassName("log-footer-inner")[0];
+            footer.insertBefore(button, footer.childNodes[0]);
+        }
+
+        return ret;
+    });
+
 })();
